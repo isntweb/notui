2ff500e9f462761131671412f20dd9644098a7f9
Jacob Chvatal <jake@isnt.online>
2021-06-02 17:06:05 -0700

    actually export all of the components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,19 @@
 import Button from './Button/Button';
 import Box from './Box/Box';
+import TextInput from './Input/TextInput';
+import Spinner from './Spinner';
+import Subtitle from './Subtitle/Subtitle';
+import Text from './Text/Text';
+import Title from './Title/Title';
+import PropTypes from "./prop-types";
 
 export {
     Button,
-    Box
+    Box,
+    TextInput,
+    Spinner,
+    Subtitle,
+    Text,
+    Title,
+    PropTypes,
 };
