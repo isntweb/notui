c4e74bdfba17225f86d8413ca86fe56508eb9830
Jacob Chvatal <jake@isnt.online>
2021-06-02 19:32:15 -0700

    babel react 17 interop

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
-// babel.config.js
-
 module.exports = function (api) {
     api.cache(true);
     
-    const presets = [ "@babel/preset-env", "@babel/preset-react" ];
+    const presets = [ "@babel/preset-env", 
+        ["@babel/preset-react", { "runtime": "automatic"}]];
     const plugins = [ "macros" ];
 
     return {
